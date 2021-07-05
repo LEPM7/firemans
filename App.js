70607092e15f11cc9a42b3f8ddb7b112b503f656
Luis Peralta <luiseduardoperaltamartinez@gmail.com>
2021-07-05 17:50:25 -0600

    added opening waze and some styling

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Alert, TouchableHighlight} from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Alert,
+  TouchableHighlight,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-import { EMERGENCY_STATES } from './models/emergency_states';
-import axios from 'axios';
-import { NavigationApps, actions, googleMapsTravelModes, mapsTravelModes } from "react-native-navigation-apps";
-
+import { EMERGENCY_STATES } from "./models/emergency_states";
+import axios from "axios";
+import {
+  NavigationApps,
+  actions,
+  googleMapsTravelModes,
+  mapsTravelModes,
+} from "react-native-navigation-apps";
+import {
+  Flex,
+  NativeBaseProvider,
+  Heading,
+  IconButton,
+  Icon,
+  Input,
+  Button,
+} from "native-base";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
-
   const [emergency, setEmergency] = useState(null);
-  const [onRoad, setOnRoad] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [validID, setValidID] = useState(false);
   const [id, setID] = useState(null);
-  const URL = 'https://br6cad6dd4.execute-api.us-east-1.amazonaws.com/dev/emergency';
+  const URL =
+    "https://br6cad6dd4.execute-api.us-east-1.amazonaws.com/dev/emergency";
 
   const getEmergency = async () => {
     setEmergency(null);
@@ -21,7 +42,7 @@ export default function App() {
       const response = await axios.get(`${URL}/${id}`);
       const data = response.data;
       if (data.errorMessage) {
-        Alert.alert('El ID de la Emergencia es incorrecto');
+        Alert.alert("El ID de la Emergencia es incorrecto");
         return;
       }
       const emergency = data;
@@ -31,79 +52,125 @@ export default function App() {
     } catch (e) {
       console.error(e);
     }
-  }
+  };
 
-  const fulfillEmergency = async () => {}
+  const fulfillEmergency = async () => {
+    setIsLoading(true);
+    try {
+      const response = await axios.put(`${URL}`, {...emergency, state: EMERGENCY_STATES.FULFILLED }, { 
+        'Content-Type': 'application/json'
+      });
+      setIsLoading(false);
+      setEmergency(null);
+      alert('Emergencia completada satisfactoriamente');
+    } catch(e){
+      alert('Hubo un error, intente mas tarde');
+      setIsLoading(false);
+      console.error(e);
+    }
+  };
+
+  const showFields = () =>
+    validID && emergency && emergency.state === EMERGENCY_STATES.ONROAD;
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text style={styles.title}>EMERGENCIA</Text>
-      <Text style={styles.Text}>ID:</Text>
-      <TextInput
-        style={styles.textInput}
-        onChangeText={text => {
-          text.replace(/[^0-9]/g, '');
-          setID(text);
-          text.trim() === '' ? setValidID(false) : setValidID(true);
-        }}
-        keyboardType='numeric'
-      />
-      {validID ?
-        <TouchableHighlight
-          style={{ ...styles.openButton, backgroundColor: "#009F03", marginTop: 25 }}
-          onPress={() => {
-            getEmergency();
-          }}
-        ><Text>Obtener Emergencia</Text></TouchableHighlight> : null}
-
-      {emergency && emergency.state === EMERGENCY_STATES.ONROAD ?
-        <View>
-          <NavigationApps
-            iconSize={50}
-            row
-            address='some default address to navigate' // address to navigate by for all apps 
-            waze={{ address: '', lat: emergency.latitude, lon: emergency.longitude, action: actions.navigateByLatAndLon }} // specific settings for waze
-            googleMaps={{ lat: emergency.latitude, lon: emergency.longitude, action: actions.navigateByLatAndLon, travelMode: googleMapsTravelModes.driving }}
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <StatusBar style="default" />
+        <Heading size="2xl">EMERGENCIA</Heading>
+        <Flex
+          flexDirection="row"
+          alignItems="center"
+          width="100%"
+          justifyContent="center"
+        >
+          <Input
+            size="sm"
+            marginRight={2}
+            w="60%"
+            placeholder="ID:"
+            onChangeText={(text) => {
+              text.replace(/[^0-9]/g, "");
+              setID(text);
+              text.trim() === "" ? setValidID(false) : setValidID(true);
+            }}
+            type="numeric"
           />
-          <TouchableHighlight
-            style={{ ...styles.openButton, backgroundColor: "#009F03", marginTop: 25 }}
-            onPress={() => fulfillEmergency()}
-          ><Text>Emergencia Completada</Text></TouchableHighlight>
-        </View> : null}
-    
+          {validID && (
+            <IconButton
+              variant="solid"
+              icon={
+                <Icon
+                  size="sm"
+                  as={<AntDesign name="search1" />}
+                  color="white"
+                />
+              }
+              onPress={() => {
+                getEmergency();
+              }}
+            />
+          )}
+        </Flex>
+        {showFields() && (
+          <Flex flexDirection="row" alignItems="center" marginTop={4} marginBottom={2}>
+            <NavigationApps
+              iconSize={50}
+              row
+              address="some default address to navigate" // address to navigate by for all apps
+              waze={{
+                address: "",
+                lat: emergency.latitude,
+                lon: emergency.longitude,
+                action: actions.navigateByLatAndLon,
+              }} // specific settings for waze
+              googleMaps={{
+                lat: emergency.latitude,
+                lon: emergency.longitude,
+                action: actions.navigateByLatAndLon,
+                travelMode: googleMapsTravelModes.driving,
+              }}
+            />
+          </Flex>
+        )}
+        {showFields() && (
+          <Button isLoading={isLoading} onPress={() => fulfillEmergency()}>
+            Completar Emergencia
+          </Button>
+        )}
       </View>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   inputID: {
-    fontSize: 10
+    fontSize: 10,
   },
   title: {
-    fontSize: 25
+    fontSize: 25,
   },
   openButton: {
     borderRadius: 20,
     padding: 10,
     elevation: 2,
     margin: 5,
-    width: '55%',
+    width: "55%",
   },
   Text: {
-    textAlign: "center"
+    textAlign: "center",
   },
   textInput: {
-    width: '70%',
+    width: "70%",
     margin: 5,
     borderWidth: 1,
     borderRadius: 20,
-    textAlign: 'center'
-  }
+    textAlign: "center",
+  },
 });
